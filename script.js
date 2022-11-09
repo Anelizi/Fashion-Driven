ab7fde1aa0344e5093874486f810947d64d3a756
anelizicarvalho <anelizicarvalho@gmail.com>
2022-11-09 15:43:22 -0300

    Implementado a função que verifica a URL

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let input = null;
+
+let modeloTitulo, golaTitulo, tecidoTitulo;
+
+//const nomeUsuario = prompt("qual seu nome?");
 
 
 //pegar a camisa que foi clicada e trazer para o javaScript
-function selecionadoModelo(camisaClicada){
+function selecionadoModelo(camisaClicada, modelo){
     // pegar o elemento que foi selecionado anteriormete
     const pratoSelicionado = document.querySelector('.caixa1 .selecionado');
     // ve se a camisa selecionada não é null
@@ -39,4 +44,36 @@ function selecionadoTecido(camisaClicada){
     }
     //adicionar a classe selecionado no elemendo que foi clicado
     camisaClicada.classList.add('selecionado');
+}
+
+//verificando se é uma URL
+function checkUrl(str){
+    //Se o que o usuario mandou for diferente de null e de string vazio
+    if(str != null && str != ''){
+        let url_ok = /(https(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+        return url_ok.test(str);
+    }else{
+        return false;
+    }
+}
+
+//função para pega o valor do input 
+const pegarUrl = document.querySelector('[data-button]');
+
+const buttonUrl = (evento) =>{
+    evento.preventDefault();
+     
+    input = document.querySelector('[data-form-input]').value;
+    // verificando se o que o usuario mandou é uma url
+    let checkInput = checkUrl(input);
+    //Colocar um if aqui
+    console.log(input);
+    input.value = '';
+}
+
+pegarUrl.addEventListener('click', buttonUrl);
+
+const ativarBotaoFecharPedido = () =>{
+    //se camisa estiver selecionada
+  
 }
